5a07eec30967b706c9748c3221d5d36f97252ee7
mudumbaid <mudumbaid@gmail.com>
2026-01-03 22:09:15 -0500

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import  DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging  
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -12,7 +13,14 @@ if __name__ == "__main__":
         data_ingestion_config = DataIngestionConfig(training_pipeline_config)
         dataingestion = DataIngestion(data_ingestion_config)
         logging.info("Starting Data Ingestion")
-        dataingestionartifact = dataingestion.initiate_data_ingestion()
-        print(dataingestionartifact)
+        data_ingestion_artifact = dataingestion.initiate_data_ingestion()
+        logging.info("Data Ingestion Completed")
+        print(data_ingestion_artifact)
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate Data Validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
---
+++ networksecurity/utils/main_utils/__init__.py
@@ -0,0 +1,6 @@
+import yaml 
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+import sys,os
+import numpy as np
+# import dill
